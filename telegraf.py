a9550e0545bada503b6d84e71ff9d0749d3b46d1
lost63 <46973287+lost63@users.noreply.github.com>
2021-11-27 08:28:21 +0300

    added email send if error

--- telegraf.py
+++ telegraf.py
@@ -1,11 +1,14 @@
 import subprocess
 import path
 import time
+import _email2
+import sys
 
 while True:
     try:
         subprocess.call(['python3.8 '+path.path+'recive_parse_email.py'],shell=True, timeout=14)
         subprocess.call(['python3.8 '+path.path+'send_parse_email.py'],shell=True,timeout=14)
-    except subprocess.TimeoutExpired:
-        pass
+    except Exception as e:
+        _email2.send_email('Ошибка', 'Exception in module telegraf, line 11' + str(e), 'test1@gmail.com')
+        sys.exit(1)
     time.sleep(30)
