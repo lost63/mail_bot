89a883a051bac90d9601b7d845f2c3dd0393c66c
lost63 <46973287+lost63@users.noreply.github.com>
2021-11-27 21:34:26 +0300

    added exception

--- telegraf.py
+++ telegraf.py
@@ -4,11 +4,18 @@ import time
 import _email2
 import sys
 
+count=0
 while True:
     try:
-        subprocess.call(['python3.8 '+path.path+'recive_parse_email.py'],shell=True, timeout=14)
-        subprocess.call(['python3.8 '+path.path+'send_parse_email.py'],shell=True,timeout=14)
+        subprocess.check_call(['python3.8 '+path.path+'recive_parse_email.py'],shell=True, timeout=14)
+        subprocess.check_call(['python3.8 '+path.path+'send_parse_email.py'],shell=True, timeout=14)
+    except subprocess.TimeoutExpired:
+        _email2.send_email('Ошибка', 'Exception timeout subprocess, module telegraf, line 11 '+str(count), 'test1@gmail.com')
+        count+=1
+        if count > 5:
+            sys.exit(1)
     except Exception as e:
         _email2.send_email('Ошибка', 'Exception in module telegraf, line 11' + str(e), 'test1@gmail.com')
         sys.exit(1)
     time.sleep(30)
+
